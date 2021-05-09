8af0b724a951966e06c1ecae432bf65a929abc60
Raphael Berger <raphaelmb@gmail.com>
2021-05-08 21:11:02 -0300

    add encrypt/decrypt

--- server.js
+++ server.js
@@ -14,15 +14,40 @@ app.use('/', (req, res) => {
 	res.render('index.html');
 });
 
+function decrypt(str) {
+    const key = 5;
+    let result = "";
+  
+    for (let i = 0; i < str.length; i++) {
+      let letter = str[i];
+  
+      if (letter.match(/[a-z]/i)) {
+        const code = str.charCodeAt(i);
+  
+        if (code >= 65 && code <= 90) {
+          letter = String.fromCharCode(((code - 65 - key) % 26) + 65);
+        }
+  
+        else if (code >= 97 && code <= 122) {
+          letter = String.fromCharCode(((code - 97 - key) % 26) + 97);
+        }
+      }
+  
+      result += letter;
+    }
+  
+    return result;
+};
+
 let messages = [];
 
 io.on('connection', (socket) => {
-	console.log(`Socket conectado: ${socket.id}`);
-
 	socket.emit('previousMessages', messages);
 
 	socket.on('sendMessage', (data) => {
-		// TODO: criptografia
+		console.log(`MENSAGEM ENCRIPTADA RECEBIDA NO SERVIDOR: ${data.message}`)
+		data.message = decrypt(data.message);
+		console.log(`MENSAGEM DECRIPTADA PELO SERVIDOR: ${data.message}`)
 		messages.push(data);
 		socket.broadcast.emit('receivedMessage', data);
 	});
